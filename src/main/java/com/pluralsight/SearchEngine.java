86c2a12174a9bdc14293706126323536e3d47d09
antbeltran <anthonybltrn@gmail.com>
2026-04-22 09:36:11 -0500

    got launch to work and log the datetime

--- src/main/java/com/pluralsight/SearchEngine.java
+++ src/main/java/com/pluralsight/SearchEngine.java
@@ -1,4 +1,38 @@
 package com.pluralsight;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
 public class SearchEngine {
+
+    static Scanner theScanner = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        try{
+            System.out.println("Press X to launch application -->");
+            String userLaunch = theScanner.nextLine();
+
+            FileWriter fileWriter = new FileWriter("src/main/resources/logs.txt");
+            BufferedWriter buffWriter = new BufferedWriter(fileWriter);
+
+            buffWriter.write("User Action Log\n");
+            if(userLaunch.equalsIgnoreCase("x")){
+
+                LocalDateTime now = LocalDateTime.now();
+                DateTimeFormatter fmt = DateTimeFormatter.ofPattern("MM-dd-yyyy  HH:mm:ss");
+                String formattedLog = now.format(fmt);
+                buffWriter.write( formattedLog + " Launched");
+            }
+
+            buffWriter.close();
+        }catch(Exception e){
+            System.out.println("Actions not logged");
+        }
+
+    }
 }
